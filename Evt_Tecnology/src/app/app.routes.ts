86946eadf43ecacafca03e82d1cb513574cee91c
FrancoAless <fleonr3@upao.edu.pe>
2024-10-09 07:50:21 +0000

    Iniciar Sesion

--- Evt_Tecnology/src/app/app.routes.ts
+++ Evt_Tecnology/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'inicio', pathMatch: 'full' },
     { path: 'registro-usuario', component: RegistrarUsuarioComponent },
+    { path: 'iniciar-sesion', component: IniciarSesionComponent },
     { path: '**', redirectTo: 'registro-usuario'}
 ];
 
