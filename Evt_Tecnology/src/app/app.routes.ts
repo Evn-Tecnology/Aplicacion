5ff0afc6c5a321ef547ba0adfc0e555881ea06b3
FrancoAless <fleonr3@upao.edu.pe>
2024-10-09 07:30:33 +0000

    Registrar Usuario

--- Evt_Tecnology/src/app/app.routes.ts
+++ Evt_Tecnology/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path: 'registro-usuario', component: RegistrarUsuarioComponent },
+    { path: '**', redirectTo: 'registro-usuario'}
+];
+
+
